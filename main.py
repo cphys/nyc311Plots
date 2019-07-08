0403c0cd2935265eedec899d7ddb5acc35b60ddd
cphys <22038435+cphys@users.noreply.github.com>
2019-07-07 20:20:40 -0400

    added functionality to allow for different variables to be selected. Fixed problems with placement of anotation text.

--- main.py
+++ main.py
@@ -5,52 +5,114 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from plotFuns import hBarPlots
 from plotFuns import cleanData
+from plotFuns import printListOfHeaders
 import matplotlib.colors as mcolors
 
 # location of file to import
 dataLoc = 'data/NYC_311.csv'
+ny311Df = pd.read_csv(dataLoc, skipinitialspace = True)
+
+# printListOfHeaders(ny311Df)
+
 # fields to import
+
+# fields = ['incident_zip','complaint_type']
 fields = ['complaint_type', 'borough']
+# fields = ['borough','complaint_type']
+# fields = ['complaint_type','incident_zip']
+
 ny311Df = pd.read_csv(dataLoc, skipinitialspace = True, usecols = fields)
 
-# Variables to be used as col and row headers.
-rowVar = fields[0]         # variable to be listed as row headers
-colVar = fields[1]         # variable to be listed as column headers
-colDr = ['Unspecified']    # column to drop from dataframe
+colDr = None   # column to drop from dataframe
 
-# Name of column to be added with will be the total of all Plots
-tot = 'Total'
+tot = 'Total' # Name of column to be added. Will be the total of all rows
 
-# Index values to be combined
-combComps = {'Heat/Hot Water' : ['Heat/Hot Water','Heating'],
-             'Paint/Plaster' : ['Paint/Plaster','Paint - Plaster'],
-             'General Construction' : ['General Construction','Construction'],
-             'Non-Construction' : ['Nonconst']}
+numRows = 7     # Number of rows for group divisions
+totTopNum = 20  # Number of rows for y-axis of large plot
+numCols = 6     # Number of columns for group divisions
+
+# Convert all zipcodes to integer values
+for field in fields:
+    if field == 'incident_zip':
+        ny311Df[field] = ny311Df[field].apply(pd.to_numeric, errors ='coerce')
+        ny311Df[field] = ny311Df[field].astype('Int32')
 
 
-cleanAndRem = False
+# Index values to be combined and/or renamed
 
-if cleanAndRem:
-    fileName = 'cleanAndRem'
-    # Clean the data using cleanData function
-    groupdDf = cleanData(ny311Df, colVar,rowVar,totNam = tot, colsToDrop = colDr, inCombVals = combComps)
+legStyle = 5
+
+if legStyle ==1:
+    legLoc2 = 'below'
+    legLoc3 = None
+
+else:  
+    legLoc2 = 'uR'
+    legLoc3 = 'uR'
+
+
+if fields[1] == 'complaint_type':
+    combRow = None
+
+    combCol = {'Heat/\nHot Water' : ['Heat/Hot Water','Heating'],
+               'Paint/\nPlaster' : ['Paint/Plaster','Paint - Plaster'],
+               'General\nConstruction' : ['General Construction','Construction'],
+               'Non-Construction' : ['Nonconst'],
+               'Unsanitary\nCondition' : ['Unsanitary Condition'],
+               'Door/\nWindow' : ['Door/Window']}
+    yAxisLab1 = fields[0]
+    yAxisLab2 = fields[0]
+    yAxisLab3 = fields[0]
+
+elif fields[0] == 'complaint_type':
+    combCol = None
+
+    combRow = {'Heat/Hot Water' : ['Heat/Hot Water','Heating'],
+                 'Paint/Plaster' : ['Paint/Plaster','Paint - Plaster'],
+                 'General Construction' : ['General Construction','Construction'],
+                 'Non-Construction' : ['Nonconst']}
+    yAxisLab1 = fields[0]
+    yAxisLab2 = ''
+    yAxisLab3 = ''
 
 else:
-    fileName = "noCleanAndRem"
-    # Clean the data using cleanData function
-    groupdDf = cleanData(ny311Df, colVar,rowVar, totNam = tot, inCombVals = combComps, cleanData = False)
+    combRow = None
+    combCol = None
+    yAxisLab1 = fields[0]
+    yAxisLab2 = ''
+    yAxisLab3 = ''
 
 
-colNams = groupdDf.columns.values
-grpNams = np.setdiff1d(colNams,[tot])
-nGrps = len(grpNams)
-topNum = 5
+
+
+# Create a dataframe with fields[0] as the rows fields[1] as the columns
+groupdDf = cleanData(ny311Df, sortVars = fields, totNam = tot, inCombVals = [combRow, combCol], colsToDrop = colDr, sortBy = [tot])
+
+
+# Sort the data and find the top 'numCols' 311 complaints city wide
+columnNames = cleanData(ny311Df, sortVars = [fields[1],fields[0]], totNam = tot, inCombVals = [combCol, combRow], colsToDrop = colDr, sortBy = [tot]).index.values[:numCols]
+
+
+# Columns are currently sorted according to highest total complaints
+# Uncomment to sort alphabetically
+# columnNames = np.sort(columnNames)
+
+groupdDf = groupdDf[np.append(columnNames, [tot])]
+
+rowNames = list(groupdDf.index)
+
+# legNams = columnNames
+
+
 ## Plot Parameters ##
 width = 8   # Total Width of Figure
 p1Width = 2 # Width of first large plot
 p2Width = 2 # Width of second large plot
-ttlLarge = 'Total New York City 311 Complaints by Categroy'
-ttlVal = 'Top {} 311 complaints by Borough'.format(topNum)
+
+ttlVal1 = 'Top {} NYC 311 Call {}s'.format(totTopNum, fields[0])
+ttlVal2 = 'Top {}s by Top {}'.format(fields[0], fields[1])
+ttlVal3 = 'Top {}s by Top {}'.format(fields[1], fields[0])
+
 xlab = 'Number of Complaints'
 # list of colors to be used in the plot
 cols = list(mcolors.TABLEAU_COLORS.values())
@@ -59,7 +121,7 @@ blCol = cols.pop(0)
 
 
 # Create the series of subplots for figure
-fig, axs = plt.subplots(nGrps, width, figsize = (16, nGrps*2))
+fig, axs = plt.subplots(numCols, width, figsize = (16, numCols*2))
 gs = axs[0,0].get_gridspec()
 
 # remove the underlying axes
@@ -71,53 +133,59 @@ for i in range(width):
 axbig1 = fig.add_subplot(gs[0:, 0:p1Width]) # first large space on right
 axbig2 = fig.add_subplot(gs[0:, (p1Width+1):(p1Width+1+p2Width)]) # large space in center
 # list of spaces on RHS
-axrs = [fig.add_subplot(gs[i, (width - 2):width]) for i in range(nGrps)]
+axrs = [fig.add_subplot(gs[i, (width - 2):width]) for i in range(numCols)]
 
 
 # Sort the items in the dataframe first by total and then by groups
-groupdDf.sort_values(by = [tot] + list(grpNams), inplace = True, ascending = False)
+# groupdDf.sort_values(by = [tot] + list(columnNames), inplace = True, ascending = False)
+groupdDf.sort_values(by = [tot], inplace = True, ascending = False)
 
 # Add large plot on LHS using 
 hBarPlots(groupdDf[tot], axbig1,
           leg = [tot],
-          pTitle = ttlLarge,
+          num = totTopNum,
+          pTitle = ttlVal1,
           xAxisLab = xlab,
-          yAxisLab = rowVar,
-          bWidth = .5,
+          yAxisLab = yAxisLab1,
           cols=[blCol],
           bwScale = 0.35
          )
 
 # Add large plot in center
-hBarPlots(groupdDf[grpNams], axbig2,
-          leg = grpNams,
-          num = topNum,
-          pTitle = ttlVal,
+hBarPlots(groupdDf[columnNames], axbig2,
+          leg = columnNames,
+          legLoc = legLoc2,
+          num = numRows,
+          pTitle = ttlVal2,
           xAxisLab = xlab,
+          yAxisLab = yAxisLab2,
+          bwScale = .4,
           cols=cols,
          )
 
 # Create row of bar plots each with a single value
 # Only show the title on the top plot and the x-axis label on the bottom
-for i in range(nGrps):
-    groupdDf.sort_values(by = [grpNams[i]],inplace = True, ascending = False)
+for i in range(numCols):
+    groupdDf.sort_values(by = [columnNames[i]], inplace = True, ascending = False)
     if i == 0:
-        ttl = ttlVal
+        ttl = ttlVal3
         xVal = xlab
-    elif i == nGrps-1:
+    elif i == numCols - 1:
         xVal = xlab
     else:
         ttl = ''
         xVal = ''
-    hBarPlots(groupdDf[grpNams[i]], axrs[i],
-              leg = [grpNams[i]],
-              num = topNum,
+    hBarPlots(groupdDf[columnNames[i]], axrs[i],
+              leg = [columnNames[i]],
+              legLoc = legLoc3,
+              num = numRows,
               cols = cols[i],
               xAxisLab = xVal,
+              yAxisLab = yAxisLab3,
               pTitle = ttl,
              ) 
 
 dirName = os.getcwd()
-plt.savefig(os.path.join(dirName, '{}_{}.png'.format(fileName, ttlLarge)), bbox_inches = 'tight') 
+plt.savefig(os.path.join(dirName, '{}.png'.format(ttlVal2).replace(' ','_')), bbox_inches = 'tight') 
 
 
--- plotFuns.py
+++ plotFuns.py
@@ -6,21 +6,26 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 mpl.style.use('ggplot') # optional: for ggplot-like style
 
-def cleanData(df, colVar, rowVar, totNam = None, colsToDrop = None, inCombVals = None, delNA = True):
+def cleanData(df, sortVars = None, totNam = None, colsToDrop = None, inCombVals = None, delNA = False, sortBy = None):
 
-    # Count number of 'rowVar' and group by 'colVar'.
-    groupdDf = df.groupby(by = [rowVar])[colVar].value_counts()
-    groupdDf = groupdDf.unstack()
+    if sortVars is not None:
+        # Count number of 'rowVar' and group by 'colVar'.
+        groupdDf = df.groupby(by = [sortVars[0]])[sortVars[1]].value_counts()
+        groupdDf = groupdDf.unstack()
 
+    else:
+        groupdDf = df
     # Variables with list of columns and rows.
     colNams = groupdDf.columns.values
     rowNams = list(groupdDf.index)
 
+
     # Rename all of the variables with first letters capitalized.
+    # and cast labels to strings
     colDict = {col: str(col).title() for col in colNams}
     rowDict = {row: str(row).title() for row in rowNams}
     groupdDf.rename(index = rowDict, columns = colDict, inplace=True)
-
+    
     # Remove columns saved in 'colsToDrop' variable
     if colsToDrop is not None:
         try:
@@ -47,36 +52,43 @@ def cleanData(df, colVar, rowVar, totNam = None, colsToDrop = None, inCombVals =
         
     print("\nCleaning deleted: {} rows\n".format(sizeBefore-sizeAfter)) 
  
-    # Combine any values passed to the 'inCombVals' dict value
+    # Combine any row values passed to the 'inCombVals' dict value
     if inCombVals is not None:
-        keys = list(inCombVals.keys())
-        for key in keys:
-            groupdDf.loc[key] = groupdDf.loc[inCombVals[key]].sum()
-            dropRows = np.setdiff1d(inCombVals[key], keys)
-            groupdDf.drop(dropRows, inplace = True)
+        if inCombVals[0] is not None:
+            rws = list(inCombVals[0].keys())
+            for rw in rws:
+                groupdDf.loc[rw] = groupdDf.loc[inCombVals[0][rw]].sum()
+                dropRows = np.setdiff1d(inCombVals[0][rw], rws)
+                groupdDf.drop(dropRows, inplace = True)
+
+        if inCombVals[1] is not None:
+            # Combine any column values passed to the 'inCombVals' dict value
+            cls = list(inCombVals[1].keys())
+            for cl in cls:
+                groupdDf[cl] = groupdDf[inCombVals[1][cl]].sum(axis = 1)
+                dropRows = np.setdiff1d(inCombVals[1][cl], cls)
+                for row in dropRows:
+                    groupdDf.drop(row, axis=1, inplace = True)
         
     # Add a column with is the total of all of the values in the df
     if totNam is not None:
         # Add a column named Total to the end of the table
         groupdDf[totNam] = groupdDf.sum(axis=1)
 
-
+    if sortBy is not None:
         # Sort columns by 'Total' and then in order of the colNams
-        groupdDf.sort_values(by = [totNam] + list(colNams),
-                             inplace = True,
-                             ascending = False
-                        )
+        groupdDf.sort_values(by = sortBy, inplace = True, ascending = False)
+
     return groupdDf
 
 
 
-def hBarPlots(df, axVal, leg = [], recCol='steelblue', num = False, fcol1 = 'white', fcol2 = 'black', fsiz = 12, fws = .01, fhs = 0.16, xAxisLab = '', yAxisLab = '', pTitle = '', bwScale = 0.2, bWidth = .75, cols = list(mcolors.TABLEAU_COLORS.values()), stkd = False):   
+def hBarPlots(df, axVal, leg = None, recCol='steelblue', num = False, fcol1 = 'white', fcol2 = 'black', fsiz = 12, fws = .01, xAxisLab = '', yAxisLab = '', pTitle = '', bwScale = 0.2, bWidth = .75, cols = list(mcolors.TABLEAU_COLORS.values()), stkd = False, legLoc = 'uR'):   
 
     if not num:
         ax = df.plot(ax = axVal, kind = 'barh', stacked = stkd, rot = 0, width = bWidth, color = cols)
     else:
         ax = df.head(int(num)).plot(ax = axVal, kind = 'barh', stacked = stkd, rot = 0, width = bWidth, color = cols) 
-
 
     # create annotations at the end of the plots
     if not stkd:
@@ -92,19 +104,21 @@ def hBarPlots(df, axVal, leg = [], recCol='steelblue', num = False, fcol1 = 'whi
 
             if width < bwScale * maxVal:
                 ax.text(width + fws * width,
-                        i.get_y() + (fhs * height),
+                        i.get_y() + (.5 * height),
                         label,
                         fontsize = fsiz,
                         color = fcol2,
-                        horizontalalignment='left')
+                        horizontalalignment='left',
+                        verticalalignment = 'center')
 
             else:
                 ax.text(width - fws * width,
-                        i.get_y() + (fhs * height),
+                        i.get_y() + (.5 * height),
                         label,
                         fontsize = fsiz,
                         color = fcol1,
-                        horizontalalignment='right')
+                        horizontalalignment='right',
+                        verticalalignment = 'center')
 
     # make the interactive mouse over give the bar title
     def format_ycursor(y):
@@ -120,7 +134,22 @@ def hBarPlots(df, axVal, leg = [], recCol='steelblue', num = False, fcol1 = 'whi
     ax.set_xlabel(xAxisLab.replace("_"," ").title())
     ax.set_ylabel(yAxisLab.replace("_"," ").title())
     ax.set_title(pTitle.replace("_"," ").title())
-    ax.legend([str(iLeg).replace("_"," ").title() for iLeg in leg])
+
+
+    if leg is not None:
+        legLen = len(leg)
+        formLeg = [str(iLeg).replace("_"," ").title() for iLeg in leg]
+        if legLoc =='below':
+            if legLen > 1:
+                ax.legend(formLeg, loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=legLen)
+            else:
+                ax.legend(formLeg, loc='upper right', fancybox=True, shadow=True)
+        elif legLoc == None:
+            pass
+        else:
+            ax.legend(formLeg, loc='upper right', fancybox=True, shadow=True)
+            
+        
     # list x-axis in scientific notation
     ax.ticklabel_format(axis='x',scilimits=(0,0))
     return ax
